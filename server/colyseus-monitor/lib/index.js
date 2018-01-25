246fedbedd1e68d8b09ed3520774bca022394cc1
Josh Kegley <Josh1289op@gmail.com>
2018-01-25 17:17:25 -0500

    Removing ignored files

--- server/colyseus-monitor/lib/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-/**
- * Created by Derwish (derwish.pro@gmail.com) on 30.07.2017.
- * License: MIT
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-var Monitor_1 = require("./Monitor");
-exports.Monitor = Monitor_1.Monitor;
-//# sourceMappingURL=index.js.map
